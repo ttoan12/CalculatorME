7d3ecf8da6fb592584125633f7c95f269b5c3544
Toàn Trần <32505254+ttoan12@users.noreply.github.com>
2019-08-14 03:53:10 +0700

    Khởi tạo app

---
+++ app/src/main/java/com/ltdd/calculator/ConverterFragment.java
@@ -0,0 +1,26 @@
+package com.ltdd.calculator;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ConverterFragment extends Fragment {
+
+    public ConverterFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_converter, container, false);
+    }
+
+}
