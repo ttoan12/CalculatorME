e5e3b0a76a27706cf5853587691704a083b76d5f
Phuong Nguyen Ngoc Uyen <uyenphuong1898@gmail.com>
2019-08-16 11:13:45 +0700

    converter updated

--- app/src/main/java/com/ltdd/calculator/CalculatorFragment.java
+++ app/src/main/java/com/ltdd/calculator/CalculatorFragment.java
@@ -14,12 +14,11 @@ public class CalculatorFragment extends Fragment {
     public CalculatorFragment() {
         // Required empty public constructor
     }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calculator, container, false);
     }
+
 
 }
