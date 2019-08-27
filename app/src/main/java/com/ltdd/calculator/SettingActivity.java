d13964035c17112a7fb0fc2cfa3cdd6d405820f0
unknown <mng1998>
2019-08-27 11:47:07 +0700

    Dark theme updated

--- app/src/main/java/com/ltdd/calculator/SettingActivity.java
+++ app/src/main/java/com/ltdd/calculator/SettingActivity.java
@@ -1,60 +1,61 @@
 package com.ltdd.calculator;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CompoundButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.Set;
+
 public class SettingActivity extends AppCompatActivity {
     SwitchCompat switchTheme;
-    Button btnLang;
     private Dialog dialog;
     boolean stateTheme, stateLang;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.darkTheme);
+        } else setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         final SharedPreferences preferences = getSharedPreferences("PREFS", 0);
         stateTheme = preferences.getBoolean("switchTheme", false);
-        stateLang = preferences.getBoolean("switchLang", false);
         switchTheme = findViewById(R.id.switchTheme);
-        btnLang = findViewById(R.id.btnLang);
-        btnLang.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog = new Dialog(SettingActivity.this);
-                dialog.setTitle("Change language");
-                dialog.setContentView(R.layout.dialoglanguage);
-                dialog.show();
-            }
-        });
+        if ((AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES)) {
+            switchTheme.setChecked(true);
+        }
+        switchTheme.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
-        switchTheme.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View view) {
-                stateTheme = !stateTheme;
-                switchTheme.setChecked(stateTheme);
-                SharedPreferences.Editor editor = preferences.edit();
-                editor.putBoolean("switchTheme", stateTheme);
-                editor.apply();
-            }
-        });
-        btnLang.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ;
-            }
-        });
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    restartApp();
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    restartApp();
+                }
 
+            }
+            public void restartApp()
+            {
+                Intent i = new Intent(getApplicationContext(), SettingActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 
 }
